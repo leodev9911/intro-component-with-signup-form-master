9895d7976e7900591b0fa41f3bbb08d2b18397e6
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-07-01 17:36:35 -0400

    Añadidas todas las funcionalidades

---
+++ src/script.js
@@ -0,0 +1,40 @@
+const inputText = document.querySelectorAll('.input-text')
+const errorEmailMessage = document.querySelector('.email')
+const inputEmail = document.querySelector('#input-email')
+const inputSubmit = document.querySelector('#input-submit')
+const errorMessage = document.querySelectorAll('.error-message')
+const errorIcon = document.querySelectorAll('.error-icon')
+
+const handleInputEmailError = () => {
+    const email = inputEmail.value
+    const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+
+    if (isValid) {
+        errorEmailMessage.classList.add('hidden');
+    } else if (!isValid) {
+        errorEmailMessage.classList.remove('hidden');
+    } 
+
+    if (inputEmail.value === '') {
+        errorEmailMessage.classList.add('hidden');
+    }
+
+}
+
+const handleInputError = () => {
+    inputText.forEach(input => {
+        if (input.value === '') {
+            input.classList.add('red')
+            errorMessage.forEach(message => {
+                message.classList.remove('hidden')
+            })
+            errorIcon.forEach(icon => {
+                icon.classList.remove('hidden')
+            })
+        } 
+    })
+}
+console.log(inputText)
+
+inputEmail.addEventListener('input', handleInputEmailError)
+inputSubmit.addEventListener('click', handleInputError)
