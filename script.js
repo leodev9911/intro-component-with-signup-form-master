83e18e18649f2da4dfc6653bcdff7b97830f5a58
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-07-01 18:13:55 -0400

    Updates

--- script.js
+++ script.js
@@ -25,17 +25,30 @@ const handleInputEmailError = () => {
 }
 
 const handleInputError = () => {
-    inputText.forEach(input => {
-        if (input.value === '') {
-            input.classList.add('red')
-            errorMessage.forEach(message => {
-                message.classList.remove('hidden')
-            })
-            errorIcon.forEach(icon => {
-                icon.classList.remove('hidden')
-            })
-        } 
-    })
+
+    if (inputText[0].value === '') {
+        errorMessage[0].classList.remove('hidden')
+        errorIcon[0].classList.remove('hidden')
+        inputText[0].classList.add('red')
+    }
+
+    if (inputText[1].value === '') {
+        errorMessage[1].classList.remove('hidden')
+        errorIcon[1].classList.remove('hidden')
+        inputText[1].classList.add('red')
+    }
+
+    if (inputText[2].value === '') {
+        errorMessage[2].classList.remove('hidden')
+        errorIcon[2].classList.remove('hidden')
+        inputText[2].classList.add('red')
+    }
+
+    if (inputText[3].value === '') {
+        errorMessage[3].classList.remove('hidden')
+        errorIcon[3].classList.remove('hidden')
+        inputText[3].classList.add('red')
+    }
 }
 console.log(inputText)
 
